f2dd8b4f9282887b4fef12f7ff4c34d67bd45e20
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-09-29 14:02:23 +0300

    midleware for getting authentication data

--- handlers/list_of_ads.py
+++ handlers/list_of_ads.py
@@ -2,13 +2,23 @@ from aiogram import F, Router, types
 
 from middlewares import auth_middlewares
 
+import pprint
+
+from typing import Dict, Any
 
 
 router = Router()
 router.message.middleware(auth_middlewares.IsAuthenticatedMiddleware())
+router.message.middleware(auth_middlewares.GetJWTAuthenticationMiddleware())
+
+
 
 
 # List of ads
 @router.message(F.text == "Список оголошень")
-async def list_of_ads_handler(message: types.Message):
+async def list_of_ads_handler(message: types.Message, middleware_access_data: Dict[str, Any] | None):
+    print('-------------MESSAGE-----DICT---------------')
+    # pprint.pprint(data)
+    print(middleware_access_data)
+    print('--------------------------------------------')
     await message.reply("Ви намагаєтеся отримати список оголошень. Функція пока не розроблена.")
--- middlewares/auth_middlewares.py
+++ middlewares/auth_middlewares.py
@@ -16,39 +16,42 @@ class IsAuthenticatedMiddleware(BaseMiddleware):
             event: Message,
             data: Dict[str, Any]
     ) -> Any:
-        # print('-------------2---------------')
-        # print(event.chat.id)
-        # print('----------STOP---------------')
-        # auth_pymongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-        # auth_db = auth_pymongo_client["rptutorial"]
-        # auth_collection = auth_db["bot_auth_collection"]
-
 
         auth_pymongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
         auth_db = auth_pymongo_client.rptutorial
         auth_collection = auth_db.bot_aut_collection
-
-
-        # user_id = auth_collection.find_one({"chat_id": event.chat.id})
         user_id = auth_collection.find_one({"chat_id": event.chat.id})
-        print('-------1--------')
-        print(auth_pymongo_client)
-        print('-------2--------')
-        print(auth_db)
-        print('-------3--------')
-        print(auth_collection)
-        print('-------4--------')
-        print('---||||||||-----')
-        print(type(event.chat.id))
-        print('-------5--------')
-        print(user_id)
-        print('-------6--------')
-        print(auth_collection.find())
-        for auth_position in auth_collection.find():
-            pprint.pprint(auth_position)
-        print('-----FINISH-----')
 
         if user_id:
             return await handler(event, data)
         else:
             return await event.answer(text="Ви не увійшли в систему. Будьласка увійдіть або зареєструйтеся.")
+        
+
+import pprint
+
+
+class GetJWTAuthenticationMiddleware(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any]
+    ) -> Any:
+        auth_pymongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+        auth_db = auth_pymongo_client.rptutorial
+        auth_collection = auth_db.bot_aut_collection
+        user_obj = auth_collection.find_one({"chat_id": event.chat.id})
+
+        try:
+            middleware_access_data = {"access_token": user_obj["access_token"],
+            "refresh_token": user_obj["refresh_token"],
+            "email": user_obj["email"]}
+            data['middleware_access_data'] = middleware_access_data
+        except TypeError:
+            pass
+
+        if user_obj:
+            return await handler(event, data)
+        else:
+            return await event.answer(text="Користувач з такими данними є заблоковним.")
