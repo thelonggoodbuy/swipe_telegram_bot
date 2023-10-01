f7319249fc310ba8111369fbec64a07028689c8b
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-10-01 19:13:26 +0300

    list of ads with pagination

---
+++ keyboards/main_keyboard.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram import types
+
+def make_main_keyboard() -> ReplyKeyboardMarkup:
+    # row = [KeyboardButton(text=item) for item in items]
+    
+    builder = ReplyKeyboardBuilder()
+    builder.adjust(2)
+    builder.add(types.KeyboardButton(
+        text='Увійти в бот'
+    ))
+    builder.add(types.KeyboardButton(
+        text='Зареєструватись'
+    ))
+    builder.add(types.KeyboardButton(
+        text='Список оголошень'
+    ))
+    return builder.as_markup(resize_keyboard=True)
