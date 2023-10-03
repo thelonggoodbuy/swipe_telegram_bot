ad1fc6d2100b2bf959293c0b074cad3e85a26bf6
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2023-10-03 16:32:06 +0300

    Images in ads feed

--- handlers/list_of_ads.py
+++ handlers/list_of_ads.py
@@ -1,15 +1,18 @@
 from aiogram import F, Router, types
-from middlewares import auth_middlewares
+from aiogram.fsm.state import State, StatesGroup
+
+from typing import Dict, Any
 
 import httpx
 import json
+import pprint
 
 from pymongo import MongoClient
 
-import pprint
-
-from typing import Dict, Any
 from keyboards.main_keyboard import make_main_keyboard
+from middlewares import auth_middlewares
+from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
+
 
 
 router = Router()
@@ -22,65 +25,58 @@ db = client.rptutorial
 bot_aut_collection = db.bot_aut_collection
 
 
+class AdsFeedState(StatesGroup):
+    total_ads = State()
+    total_ads_quantity = State()
+    current_ads_index = State()
+
 
 # List of ads
 @router.message(F.text == "Список оголошень")
 async def list_of_ads_handler(message: types.Message, middleware_access_data: Dict[str, Any] | None):
 
-    # await message.reply("Ви намагаєтеся отримати список оголошень. Функція пока не розроблена.")
-    # await message.reply(middleware_access_data)
-
     await message.reply("Всі оголошення разом:")
 
     auth_data = bot_aut_collection.find_one({"chat_id": message.chat.id})
 
-    print('----------------------')
-    print(auth_data['access_token'])
-    print('----------------------')
-
     # in doc header here
     with httpx.Client() as client:
         url = "http://127.0.0.1:8000/ads/ads-feed/"
-
         client.headers['Authorization'] = f"Bearer {auth_data['access_token']}"
-
-        # data = {"email": auth_data['users_email'], "password": auth_data['users_password']}
-
-        # (client)
-
         response = client.get(url, timeout=10.0)
-
-
-
-        # response_dict = json.loads(response.text)
-
         match response.status_code:
             case 200:
-
-                # auth_object = {"chat_id": message.chat.id,
-                #                "email": response_dict["email"],
-                #                "access_token": response_dict["tokens"]["access"],
-                #                "refresh_token": response_dict["tokens"]["refresh"]}
-                
-                # bot_aut_collection.update_one({"chat_id": message.chat.id},\
-                #                                {"$set": auth_object}, upsert=True)
-                
-                # response_text = f"Ви увійшли в бот як {response_dict['email']}"
-                # await message.answer(
-                # text = str(response.text)
-                # )
-
-                print('------------------------------------------------------------------------')
-                pprint.pprint(response.text)
-                print(type(response.text))
                 result = json.loads(response.text)
                 print('========================================================================')
                 print(result)
                 print('------------------------------------------------------------------------')
+
+                basic_url = 'http://127.0.0.1:8000'
+
+                for ads in result:
+                    await message.answer(
+                        text=str(ads)
+                    )
+                    await message.answer(
+                        text = f"link to image is { basic_url + ads['accomodation_data']['main_image']}"
+                    )
+                    image_url = f"{ basic_url + ads['accomodation_data']['main_image']}"
+
+                    image_from_url = URLInputFile(image_url)
+
+                    await message.answer_photo(
+                        image_from_url,
+                        caption="Зображення будинку"
+                    )
+
+                
                 await message.answer(
                 text = "Все ок!",
                 reply_markup=make_main_keyboard()
                )
+
+
+
                 
             case 400:
                 response_text = response.text
@@ -90,11 +86,8 @@ async def list_of_ads_handler(message: types.Message, middleware_access_data: Di
                 )
 
             case 401:
-                print('-----401')
                 data = {'refresh': auth_data['refresh_token']}
-
                 url = "http://127.0.0.1:8000/api/token/refresh/"
-
                 response = client.post(url, data=data, timeout=10.0)
 
                 if response.status_code == 200:
@@ -111,5 +104,4 @@ async def list_of_ads_handler(message: types.Message, middleware_access_data: Di
 
 
                 if response.status_code == 401:
-                    print('=====SMTH===WITH====REFRESH=====')
                     print(response.text)
